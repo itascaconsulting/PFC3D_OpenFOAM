e6411b28857b542e86bbb83040a280af5af57e80
Jason Furtney <jkfurtney@gmail.com>
2016-05-25 12:38:35 -0500

    first pass at porous flow test case

---
+++ porous1/pfc_porous1.py
@@ -0,0 +1,55 @@
+import itasca as it
+from itasca import cfdarray as ca
+from itasca.util import p2pLinkServer
+
+import numpy as np
+
+with p2pLinkServer() as cfd_link:
+    cfd_link.start()
+
+    nodes = cfd_link.read_data()
+    elements = cfd_link.read_data()
+    fluid_density = cfd_link.read_data()
+    fluid_viscosity = cfd_link.read_data()
+    print fluid_density, fluid_viscosity
+    nmin, nmax = np.amin(nodes,axis=0), np.amax(nodes,axis=0)
+    diag = np.linalg.norm(nmin-nmax)
+    dmin, dmax = nmin -0.1*diag, nmax+0.1*diag
+    print dmin, dmax
+    it.command("""
+    new
+    domain extent {} {} {} {} {} {}
+    """.format(dmin[0], dmax[0],
+               dmin[1], dmax[1],
+               dmin[2], dmax[2]))
+    ca.create_mesh(nodes, elements)
+    it.command("""
+    config cfd
+    element cfd ini density {}
+    element cfd ini visc {}
+    cfd porosity poly
+    cfd buoy on
+    cfd update
+    call particles.p3dat
+    """.format(fluid_density, fluid_viscosity))
+
+
+    element_volume = ca.volume()
+    dt = 0.005
+
+    for i in range(100):
+        it.command("solve age {}".format(it.mech_age()+dt))
+        print "sending solve time"
+        cfd_link.send_data(dt) # solve interval
+        cfd_link.send_data(ca.porosity())
+        cfd_link.send_data((ca.drag().T/element_volume).T/fluid_density)
+        print " cfd solve started"
+        ca.set_pressure(cfd_link.read_data())
+        ca.set_pressure_gradient(cfd_link.read_data())
+        ca.set_velocity(cfd_link.read_data())
+        print " cfd solve ended"
+
+    cfd_link.send_data(0.0)
+
+    print "ball z velocity", it.ball.find(1).vel_z()
+it.command('plot bitmap filename "porous1.png"')
