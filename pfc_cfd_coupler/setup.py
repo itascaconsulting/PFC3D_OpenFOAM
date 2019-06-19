6f052d568139411565a7668ef1a03c0a9c6a98cf
Grisha Drozdov <drozdovgrigoriy@gmail.com>
2019-06-19 12:57:38 -0500

    added setup.py to allow install

---
+++ pfc_cfd_coupler/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pfc_cfd_coupler',
+      version='0.1',
+      description='PFC3D 5 - OpenFoam 3+ coupler',
+      url='http://github.com/drozdovgrigoriy/PFC3D_OpenFOAM/pfc_cfd_coupler',
+      author='Grisha Drozdov',
+      author_email='drozdovgrigoriy@gmail.com',
+      license='MIT',
+      packages=['pfc_cfd_coupler'])
