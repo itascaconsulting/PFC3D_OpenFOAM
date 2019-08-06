713ae9c0dfc296259b9831a23cb1c4c5b73b7f1a
Grisha Drozdov <drozdovgrigoriy@gmail.com>
2019-08-05 19:53:56 -0500

    added fluidized_bed_1 to compare with previous cfd

---
+++ fluidized_bed_1/pfc_fluidized_bed_1.py
@@ -0,0 +1,67 @@
+import itasca as it
+from itasca import cfdarray as ca
+from itasca.util import p2pLinkServer
+
+import numpy as np
+
+cfd_link = p2pLinkServer()
+cfd_link.start()
+
+nodes = cfd_link.read_data()
+elements = cfd_link.read_data()
+fluid_density = cfd_link.read_data()
+fluid_viscosity = cfd_link.read_data()
+print fluid_density, fluid_viscosity
+nmin, nmax = np.amin(nodes,axis=0), np.amax(nodes,axis=0)
+diag = np.linalg.norm(nmin-nmax)
+dmin, dmax = nmin -0.1*diag, nmax+0.1*diag
+print dmin, dmax
+
+it.command("""
+new
+domain extent {} {} {} {} {} {}
+""".format(dmin[0], dmax[0],
+           dmin[1], dmax[1],
+           dmin[2], dmax[2]))
+ca.create_mesh(nodes, elements)
+it.command("""
+config cfd
+set timestep max 1e-5
+element cfd ini density {}
+element cfd ini visc {}
+;cfd porosity poly
+cfd buoy on
+ball create rad 0.005 x 0.5 y 0.5 z 0.5
+ball ini dens 2500
+ball prop kn 1e2 ks 1e2 fric 0.25
+set gravity 0 0 -9.81
+def fluid_time
+  global fluid_time = mech.age
+end
+history add id 1 fish @fluid_time
+ball history id 2 zvelocity id 1
+ball history id 3 zunbalforce id 1
+ball cfd history id 4 zforce id 1
+plot clear
+plot add hist 2 vs 1
+plot add cfdelement shape arrow colorby vectorattribute "velocity"
+""".format(fluid_density, fluid_viscosity))
+
+element_volume = ca.volume()
+dt = 0.005
+
+for i in range(100):
+    it.command("solve age {}".format(it.mech_age()+dt))
+    cfd_link.send_data(dt) # solve interval
+    cfd_link.send_data(ca.porosity())
+    cfd_link.send_data((ca.drag().T/element_volume).T/fluid_density)
+    ca.set_pressure(cfd_link.read_data())
+    ca.set_pressure_gradient(cfd_link.read_data())
+    ca.set_velocity(cfd_link.read_data())
+
+cfd_link.send_data(0.0) # solve interval
+cfd_link.close()
+del cfd_link
+
+print "ball z velocity", it.ball.find(1).vel_z()
+it.command("history write 1,2,3,4 file 'droptest1.txt' truncate")
---
+++ fluidized_bed_2/pfc_fluidized_bed_2.py
@@ -0,0 +1,34 @@
+import itasca as it
+from pfc_cfd_coupler.pfc_coupler import pfc_coupler
+
+coupler = pfc_coupler()
+it.command("""
+ball generate rad 0.005 number 100 box 0 1 0 0.25 0 1
+ball ini dens 2500
+ball prop kn 1e2 ks 1e2 fric 0.25
+set gravity 0 -9.81 0
+def ball_height
+  local max = 0
+  loop foreach local b ball.list
+    if ball.pos.y(b) > max then
+      max = ball.pos.y(b)
+    endif
+  endloop
+  ball_height = max
+end
+def fluid_time
+  global fluid_time = mech.age
+end
+history add id 1 fish @ball_height
+history add id 2 fish @fluid_time
+plot clear
+plot add hist 1 vs 2
+plot add ball
+plot add udvector
+""")
+
+coupler.dt = 0.005
+coupler.bandwidth = 0
+coupler.solve(200)
+coupler.plotFluidUnitVel()
+coupler.close()
