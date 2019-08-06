73eae4c2dabd017ce8ef559608375cfc05331c04
Grisha Drozdov <drozdovgrigoriy@gmail.com>
2019-08-06 16:23:50 -0500

    merged frontAndBack walls to fixedWalls

--- fluidized_bed_2/cfd_fluidized_bed_2.py
+++ fluidized_bed_2/cfd_fluidized_bed_2.py
@@ -4,28 +4,31 @@ from pyDemFoam import pyDemIcoFoam
 
 solver = pyDemIcoFoam()
 
-with p2pLinkClient() as pfc_link:
-    #pfc_link.connect("10.0.2.2") # for VirtualBox
-    pfc_link.connect("")        # for WSL
+pfc_link = p2pLinkClient()
+#pfc_link.connect("10.0.2.2") # for VirtualBox
+pfc_link.connect("")        # for WSL
 
-    pfc_link.send_data(solver.nodes())
-    pfc_link.send_data(solver.elements())
-    pfc_link.send_data(solver.cell_centers())
-    pfc_link.send_data(solver.cell_volumes())
-    pfc_link.send_data(solver.rho())
-    pfc_link.send_data(solver.mu())
+pfc_link.send_data(solver.nodes())
+pfc_link.send_data(solver.elements())
+pfc_link.send_data(solver.cell_centers())
+pfc_link.send_data(solver.cell_volumes())
+pfc_link.send_data(solver.rho())
+pfc_link.send_data(solver.mu())
 
-    while True:
-        deltat = pfc_link.read_data()
-        if deltat == 0.0:
-            print "solve finished"
-            break
-        solver.n(pfc_link.read_data())
-        solver.f(pfc_link.read_data())
-        
-        solver.set_dt(deltat)
-        solver.solve(deltat)
-        
-        pfc_link.send_data(solver.p()*solver.rho())
-        pfc_link.send_data(solver.gradp()*solver.rho())
-        pfc_link.send_data(solver.U())
+while True:
+    deltat = pfc_link.read_data()
+    if deltat == 0.0:
+        print "solve finished"
+        break
+    solver.n(pfc_link.read_data())
+    solver.f(pfc_link.read_data())
+
+    solver.set_dt(deltat)
+    solver.solve(deltat)
+
+    pfc_link.send_data(solver.p()*solver.rho())
+    pfc_link.send_data(solver.gradp()*solver.rho())
+    pfc_link.send_data(solver.U())
+
+pfc_link.close()
+del pfc_link
