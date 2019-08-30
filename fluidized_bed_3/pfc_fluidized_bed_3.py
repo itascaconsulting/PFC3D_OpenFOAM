c341d516ff9b28094873d9c990ff32c06523c80d
Grisha Drozdov <drozdovgrigoriy@gmail.com>
2019-08-30 14:00:25 -0500

    changed viscosity and raised balls above flow

--- fluidized_bed_3/pfc_fluidized_bed_3.py
+++ fluidized_bed_3/pfc_fluidized_bed_3.py
@@ -4,14 +4,14 @@ from pfc_cfd_coupler.pfc_coupler import pfc_coupler
 coupler = pfc_coupler()
 it.command("call ../fluidized_bed_3/make_ini.p3dat")
 
-coupler.max_dt = 0.00001
-coupler.bandwidth = 0
+coupler.max_dt = 0.001
 coupler.smallest_size = 0.00095
+coupler.bandwidth = 2*coupler.smallest_size
 coupler.initialize()
-coupler.solve(2.0)
+coupler.solve(4.0)
 coupler.plotFluidVel()
 coupler.plotPorosity()
 coupler.stopSolve()
 coupler.close()
 
-#it.command("history write 1,2,3 file 'fluidized_bed_2.txt' truncate")
+it.command("history write 1,2 file 'fluidized_bed_3.txt' truncate")
